7ffbbf2acccfeca0cf89cdc6aab2175f83c5b136
Justin <jlkris007@gmail.com>
2023-01-10 03:14:54 -0500

    Stop role creation and syncing of perms.

--- bot/cogs/manage_classes_cog.py
+++ bot/cogs/manage_classes_cog.py
@@ -135,13 +135,13 @@ class ManageClassesCog(commands.Cog):
         await channel.send(f"Here is your generated class channel {ctx.author.mention}, Good luck!")
 
         # create a class role and mark it as assignable
-        role = await self.create_role(ctx, class_repr)
+        # role = await self.create_role(ctx, class_repr)
 
         # Sleep here to make sure the role has been sent to the database and added
-        await asyncio.sleep(0.5)
+        # await asyncio.sleep(0.5)
 
         # sync perms with cleanup role
-        await self.sync_perms(ctx, channel, role)
+        # await self.sync_perms(ctx, channel, role)
 
     async def input_class(self, ctx, class_repr: ClassType) -> ClassType | None:
         def input_check(msg: discord.Message) -> bool:
