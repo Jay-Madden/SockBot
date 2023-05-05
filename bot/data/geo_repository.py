34bb03e7025a778fc68a83c8730549c6fae0c4b8
vineetsaraf <tcw7711@gmail.com>
2023-05-05 18:00:57 -0400

    Removed unnecessary methods in geo_repository.py

--- bot/data/geo_repository.py
+++ bot/data/geo_repository.py
@@ -1,26 +1,26 @@
 import aiosqlite
 from bot.data.base_repository import BaseRepository
-from bot.models.class_models import GeoguessrLeaderboard as leaderboardDC
+from bot.models.class_models import GeoguessrLeaderboard as GeoguessrLeaderboard
 
 
 class geo_repository(BaseRepository):
 
-    async def get_rank(self) -> list[leaderboardDC]:
+    async def get_rank(self) -> list[GeoguessrLeaderboard]:
         async with aiosqlite.connect(self.resolved_db_path) as connection:
             cursor = await connection.execute('SELECT *, ROW_NUMBER() OVER(ORDER BY score DESC) AS RANK from GeoguessrLeaderboard')
             return await self.fetch_all_as_dict(cursor)
 
-    async def return_size(self) -> list[leaderboardDC]:
+    async def return_size(self) -> int:
         async with aiosqlite.connect(self.resolved_db_path) as connection:
             cursor = await connection.execute('SELECT COUNT(*) FROM GeoguessrLeaderboard;')
-            return await self.fetch_all_as_dict(cursor)
+            return int(await self.fetch_first_as_dict(cursor)['COUNT(*)'])
 
     async def update_score(self, score, user_id):
         async with aiosqlite.connect(self.resolved_db_path) as connection:
             await connection.execute('UPDATE GeoguessrLeaderboard SET score = ? WHERE user_id = ?;', (score, user_id,))
             await connection.commit()
 
-    async def sort_and_return(self) -> list[leaderboardDC]:
+    async def sort_and_return(self) -> list[dict]:
         async with aiosqlite.connect(self.resolved_db_path) as connection:
             cursor = await connection.execute('SELECT * FROM GeoguessrLeaderboard ORDER BY score DESC LIMIT 10;')
             return await self.fetch_all_as_dict(cursor)
@@ -33,9 +33,9 @@ class geo_repository(BaseRepository):
                 return None
             return dictionary
 
-    async def check_if_user_exists(self, user_id) -> list[leaderboardDC]:
+    async def check_if_user_exists(self, user_id) -> list[dict]:
         async with aiosqlite.connect(self.resolved_db_path) as connection:
-            cursor = await connection.execute('SELECT COUNT(*) FROM GeoguessrLeaderboard WHERE user_id = :userid', {"userid": user_id})
+            cursor = await connection.execute('SELECT COUNT(*) FROM GeoguessrLeaderboard WHERE user_id = ?', (user_id,))
             return await self.fetch_all_as_dict(cursor)
 
     async def add_into(self, user_id, score):
@@ -43,21 +43,6 @@ class geo_repository(BaseRepository):
             await connection.execute('INSERT INTO GeoguessrLeaderboard (user_id, score) values (?, ?);', (user_id, score))
             await connection.commit()
 
-    #delete a user
-    async def reset(self, user_id):
-        async with aiosqlite.connect(self.resolved_db_path) as connection:
-            await connection.execute('DELETE FROM GeoguessrLeaderboard WHERE user_id = ?', (user_id,) )
-            await connection.commit()
-
-    async def get_all_members(self) -> list[leaderboardDC]:
-        async with aiosqlite.connect(self.resolved_db_path) as connection:
-            cursor = await connection.execute('SELECT * FROM GeoguessrLeaderboard;')
-            return [leaderboardDC(**d) for d in await self.fetch_all_as_dict(cursor)]
-
-    #async def get_members_by_name(self, name) -> list[leaderboardDC]:
-    #    async with aiosqlite.connect(self.resolved_db_path) as connection:
-    #        cursor = await connection.execute('SELECT * FROM GeoguessrLeaderboard WHERE name = ?;', (name,))
-    #        return [leaderboardDC(**d) for d in await self.fetch_all_as_dict(cursor)]
 
     async def get_best_preparation_for_member(self, user_id):
         async with aiosqlite.connect(self.resolved_db_path) as connection:
@@ -65,4 +50,5 @@ class geo_repository(BaseRepository):
             dictionary = await self.fetch_first_as_dict(cursor)
             if not dictionary:
                 return None
-            return leaderboardDC(**dictionary)
+            return GeoguessrLeaderboard(**dictionary)
+
